0c7fbfd1e17bd52195160bc89561e9d0226c2e71
farooq156 <farooq.regbits@gmail.com>
2025-01-29 16:15:35 +0500

    Add Syllabus Automater

--- tests/demo-video/demoVideo.spec.js
+++ tests/demo-video/demoVideo.spec.js
@@ -362,10 +362,163 @@ test.describe("test case for demo video", () => {
       .click();
     await page.getByPlaceholder("Enter Identity No").click();
     await page.getByPlaceholder("Enter Identity No").fill("67897653678");
-    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole("button", { name: "Save" }).click();
 
     // all users page
-    await page.getByRole('link', { name: 'All Users' }).click();
-    await page.waitForTimeout(2000)
+    await page.getByRole("link", { name: "All Users" }).click();
+    await page.waitForTimeout(2000);
+    // add classes
+    await page.getByRole("link", { name: "Classes" }).click();
+    await page.getByRole("button", { name: "Add Class" }).click();
+    await page.getByPlaceholder("Class Title").click();
+    await page.getByPlaceholder("Class Title").fill("9 class");
+    await page.getByPlaceholder("Class Description").click();
+    await page.getByPlaceholder("Class Description").fill("boys 9th class");
+    await page.getByRole("button", { name: "Save" }).click();
+    //edit class
+    await page
+      .getByRole("row", { name: "class boys 9th class N/A" })
+      .getByRole("img")
+      .click();
+    await page.waitForLoadState("domcontentloaded", { timeout: 20000 });
+    await page.getByText("Edit").click();
+    await page.getByPlaceholder("Class Title").click();
+    await page.getByPlaceholder("Class Title").fill("9th class");
+    await page.getByRole("button", { name: "Update" }).click();
+
+    //add room
+    await page.getByRole("link", { name: "Rooms" }).click();
+    await page.getByRole("button", { name: "Add Room" }).click();
+    await page.getByPlaceholder("Room Title").click();
+    await page.getByPlaceholder("Room Title").fill("Room 1");
+    await page.getByRole("button", { name: "Save" }).click();
+    //add section
+    await page.getByRole("link", { name: "Sections" }).click();
+    await page.getByRole("button", { name: "Add Section" }).click();
+    await page.getByPlaceholder("Section Name").click();
+    await page.getByPlaceholder("Section Name").fill("a section");
+    await page.getByPlaceholder("Section Description").click();
+    await page.getByPlaceholder("Section Description").fill("section for boys");
+    await page
+      .locator("div")
+      .filter({ hasText: /^Select$/ })
+      .nth(2)
+      .click();
+    await page.getByText("9th class").click();
+    await page.getByRole("button", { name: "Save" }).click();
+    // add existing user as teacher
+    await page
+      .locator("div")
+      .filter({ hasText: /^Users$/ })
+      .nth(1)
+      .click();
+    await page.getByRole("link", { name: "Teachers" }).click();
+    await page.getByRole("button", { name: "Add Existing User" }).click();
+    await page.waitForLoadState("domcontentloaded", { timeout: 20000 });
+    await page
+      .locator("div")
+      .filter({ hasText: /^Select$/ })
+      .nth(2)
+      .click();
+    await page.locator("li").filter({ hasText: "ANali nawaz" }).click();
+    await page.waitForLoadState("domcontentloaded", { timeout: 20000 });
+    await page
+      .locator("div:nth-child(2) > .min-w-1 > section > div > div")
+      .first()
+      .click();
+    await page.locator("li").filter({ hasText: "9th class" }).click();
+    await page.waitForLoadState("domcontentloaded", { timeout: 20000 });
+    await page
+      .locator("div")
+      .filter({ hasText: /^Select$/ })
+      .nth(2)
+      .click();
+    await page.locator("li").filter({ hasText: "a section" }).click();
+    await page.getByRole("button", { name: "Save" }).click();
+    // Add Syllabus Automater
+    await page
+      .locator("div")
+      .filter({ hasText: /^Syllabus Automater$/ })
+      .nth(1)
+      .click();
+    await page.getByRole("link", { name: "Course Provider" }).click();
+    await page.getByRole("button", { name: "Add Course" }).click();
+    await page.getByPlaceholder("Course Name").click();
+    await page.getByPlaceholder("Course Name").fill("Fedral Board");
+    await page.getByPlaceholder("Course Description").click();
+    await page
+      .getByPlaceholder("Course Description")
+      .fill("Information about the courses and deatil");
+    await page.getByRole("button", { name: "Create" }).click();
+    //Add subject syllabus template
+    await page.getByRole("link", { name: "Subject Syllabus Template" }).click();
+    await page.getByRole("button", { name: "Add New Template" }).click();
+    await page
+      .locator("div")
+      .filter({ hasText: /^Select$/ })
+      .nth(2)
+      .click();
+    await page.locator("li").filter({ hasText: "Fedral Board" }).click();
+    await page.getByPlaceholder("Subject Title").click();
+    await page.getByPlaceholder("Subject Title").fill("Computer Science");
+    await page.getByPlaceholder("Title", { exact: true }).click();
+    await page.getByPlaceholder("Title", { exact: true }).fill("OOP");
+    await page.getByPlaceholder("Grade").click();
+    await page.getByPlaceholder("Grade").fill("Tenth");
+    await page.getByPlaceholder("Year/Edition").click();
+    await page.getByPlaceholder("Year/Edition").fill("2024");
+    await page.getByRole("button", { name: "Create" }).click();
+    await page.getByRole("cell", { name: "OOP" }).click();
+    await page.getByText("OOP").click();
+    await page.getByRole("button", { name: "Add Chapters" }).click();
+    await page.getByPlaceholder("Chapter Title").click();
+    await page.getByPlaceholder("Chapter Title").fill("Intro");
+    await page.getByPlaceholder("Description").click();
+    await page
+      .getByPlaceholder("Description")
+      .fill("Introduction to the chapter");
+    await page.getByPlaceholder("Chapter Number").click();
+    await page.getByPlaceholder("Chapter Number").fill("1");
+    await page.getByRole("button", { name: "Create" }).click();
+    await page.getByRole("button", { name: "Add Topics" }).click();
+    await page
+      .locator("div")
+      .filter({ hasText: /^Select$/ })
+      .nth(2)
+      .click();
+    await page.getByText("Intro").nth(2).click();
+    await page.getByPlaceholder("Title").click();
+    await page.getByPlaceholder("Title").fill("First");
+    await page.getByPlaceholder("Description").click();
+    await page.getByPlaceholder("Description").fill("Science");
+    await page.getByPlaceholder("Topic Number").click();
+    await page.getByPlaceholder("Topic Number").fill("1");
+    await page.getByRole("button", { name: "Create" }).click();
+    await page.getByRole("button", { name: "Add Lesson" }).click();
+    await page
+      .locator(".flex > div > .min-w-1 > section > div > div > .w-full")
+      .first()
+      .click();
+    await page.locator("li").filter({ hasText: "Theory" }).click();
+    await page
+      .locator("div:nth-child(2) > .min-w-1 > section > div > div")
+      .first()
+      .click();
+    await page.getByText("Intro").nth(3).click();
+    await page
+      .locator("div")
+      .filter({ hasText: /^Select$/ })
+      .nth(3)
+      .click();
+    await page.getByText("First").nth(1).click();
+    await page.getByPlaceholder("Title").click();
+    await page.getByPlaceholder("Title").fill("Managnment");
+    await page.getByPlaceholder("Description").click();
+    await page
+      .getByPlaceholder("Description")
+      .fill("Understanding the role of managnment in our life");
+    await page.getByPlaceholder("Lesson Number").click();
+    await page.getByPlaceholder("Lesson Number").fill("1");
+    await page.getByRole("button", { name: "Create" }).click();
   });
 });
