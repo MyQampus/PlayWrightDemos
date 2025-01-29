9b4644ad919b839c4bc6923434e0b6fe09ccf280
adeelmanzoorrajput <adeel.manzoor@regbits.com>
2025-01-29 18:48:03 +0500

    Add non gpa grading structure flow

--- tests/demo-video/demoVideo.spec.js
+++ tests/demo-video/demoVideo.spec.js
@@ -520,5 +520,54 @@ test.describe("test case for demo video", () => {
     await page.getByPlaceholder("Lesson Number").click();
     await page.getByPlaceholder("Lesson Number").fill("1");
     await page.getByRole("button", { name: "Create" }).click();
+
+    // add grading structure flow
+    await page
+      .locator("div")
+      .filter({ hasText: /^Grading Structures$/ })
+      .nth(1)
+      .click();
+    await page.getByRole("link", { name: "Non GPA Grading" }).click();
+    await page.getByRole("button", { name: "Add Structure" }).click();
+    await page.getByPlaceholder("Title", { exact: true }).click();
+    await page
+      .getByPlaceholder("Title", { exact: true })
+      .fill("PassAndFail Grading");
+    await page.getByPlaceholder("Description").click();
+    await page
+      .getByPlaceholder("Description")
+      .fill(
+        "A simple grading structure where students are evaluated based on whether they meet the required criteria."
+      );
+    await page.locator('input[name="GRADE_TITLE \\+ 0"]').click();
+    await page.locator('input[name="GRADE_TITLE \\+ 0"]').fill("A");
+    await page.getByRole("button", { name: "Add New Grade" }).click();
+    await page.locator('input[name="GRADE_TITLE \\+ 1"]').click();
+    await page.locator('input[name="GRADE_TITLE \\+ 1"]').fill("B");
+    await page.locator('input[name="GRADE_VAL \\+ 1"]').click();
+    await page.locator('input[name="GRADE_VAL \\+ 1"]').fill("80");
+    await page.getByRole("button", { name: "Add New Grade" }).click();
+    await page.locator('input[name="GRADE_TITLE \\+ 2"]').click();
+    await page.locator('input[name="GRADE_TITLE \\+ 2"]').fill("C");
+    await page.locator('input[name="GRADE_VAL \\+ 2"]').click();
+    await page.locator('input[name="GRADE_VAL \\+ 2"]').fill("60");
+    await page.locator('input[name="GRADE_TITLE \\+ 3"]').click();
+    await page.locator('input[name="GRADE_TITLE \\+ 3"]').fill("F");
+    await page.getByRole("button", { name: "Save" }).click();
+    await page.waitForTimeout(1000);
+    await page.locator(".w-full > div > div > .w-full").first().click();
+    await page.locator("li").filter({ hasText: "9th class" }).click();
+    await page
+      .locator("div")
+      .filter({ hasText: /^Exam Result$/ })
+      .nth(1)
+      .click();
+    await page.getByRole("link", { name: "Subject Result" }).click();
+    await page.getByRole("button", { name: "Add Subject Result" }).click();
+    await page
+      .locator("div:nth-child(2) > .min-w-1 > section > div > div")
+      .first()
+      .click();
+    await page.getByRole("button", { name: "Cancel" }).click();
   });
 });
