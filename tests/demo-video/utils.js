47ffdf682d9ff3f8b30f2b42ce4d82781487b37e
adeelmanzoorrajput <adeel.manzoor@regbits.com>
2025-02-14 16:13:21 +0500

    Update funtion for same lable like in grading structure

--- tests/demo-video/utils.js
+++ tests/demo-video/utils.js
@@ -4,14 +4,24 @@ export async function fillInputAfterLabel(
   inputText,
   fieldType
 ) {
-  const label = await page.locator(`label:text("${labelText}")`);
-  const labelSiblingDiv = await label
-    .locator("xpath=following-sibling::div")
-    .first();
-  await labelSiblingDiv.click();
-  if ((fieldType === "text")) {
-    await labelSiblingDiv.locator("input").fill(`${inputText}`);
-  } else if ((fieldType === "singleSelect")) {
-    await page.getByText(`${inputText}`).click();
+  labelText = labelText.trim();
+  const labels = await page.locator(`label:text("${labelText}")`).all(); // Get all matching labels
+
+  for (const label of labels) {
+    const labelSiblingDiv = await label
+      .locator("xpath=following-sibling::div")
+      .first();
+    await labelSiblingDiv.click();
+
+    if (fieldType === "text") {
+      const input = labelSiblingDiv.locator("input");
+      const existingValue = await input.evaluate((el) => el.value.trim()); // Get current value
+
+      if (!existingValue) {
+        await input.fill(inputText);
+      }
+    } else if (fieldType === "singleSelect") {
+      await page.getByText(inputText).click();
+    }
   }
 }
